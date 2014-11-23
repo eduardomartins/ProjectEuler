bb8f16d1e3fd9c390a3cafeca7549d68975c33ae
Eduardo Sant'Anna Martins <eduardomartins993@hotmail.com>
2014-11-23 13:06:46 -0200

    Refatoraçao do programa substituindo recursividade por loops

--- Python/problem3.py
+++ Python/problem3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#  problem3.py
+# problem3.py
 #  
 #  Copyright 2014 Eduardo Sant'Anna Martins <eduardomartins993@hotmail.com>
 #  
@@ -26,30 +26,35 @@
 #  What is the largest prime factor of the number 600851475143 ?
 
 
-def prime(n, k=2):
+def prime(n):
     if n == 1 or n == 2:
         return True
-    if k < n/2:
-        if n % k != 0:
-            return prime(n, k+1)
-        else:
-            return False
+    k = 2
+    while k < n / 2 and n % k != 0:
+        k += 1
     else:
         return True
+    return False
 
 
-def largest_prime_factor(n, k=2):
-    if n == 1:
-        return k
+def largest_prime_factor(n):
+    k = 2
+    while k <= n:
+        print n, k
+        if prime(k) and n % k == 0:
+            n /= k
+            if n == 1:
+                return k
+        k += 1
 
-    if prime(k) and n % k == 0:
-        return largest_prime_factor(n/k, k)
-    else:
-        return largest_prime_factor(n, k+1)
+    return False
 
 
 def main():
-    print largest_prime_factor(13195)
+    num = 600851475143
+    print 'The largest factor prime is:', num, largest_prime_factor(num)
+    return 0
+
 
 if __name__ == '__main__':
     main()
