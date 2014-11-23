5fa6b49f6ea5a8d2f4b3acfb5fa2d7e41fd24e05
Eduardo Sant'Anna Martins <eduardomartins993@hotmail.com>
2014-11-23 02:59:09 -0200

    Solução do terceiro problema

--- Python/problem3.py
+++ Python/problem3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#  sem título.py
+#  problem3.py
 #  
-#  Copyright 2014 Eduardo Sant Anna Martins <eduardo@eduardo-notebook>
+#  Copyright 2014 Eduardo Sant'Anna Martins <eduardomartins993@hotmail.com>
 #  
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -19,15 +19,38 @@
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-#  
-#  
+#  ===================================================================
+#  Largest prime factor
+#  Problem 3
+#  The prime factors of 13195 are 5, 7, 13 and 29.
+#  What is the largest prime factor of the number 600851475143 ?
 
+
+def prime(n, k=2):
+    if n == 1 or n == 2:
+        return True
+    if k < n/2:
+        if n % k != 0:
+            return prime(n, k+1)
+        else:
+            return False
+    else:
+        return True
+
+
+def largest_prime_factor(n, k=2):
+    if n == 1:
+        return k
+
+    if prime(k) and n % k == 0:
+        return largest_prime_factor(n/k, k)
+    else:
+        return largest_prime_factor(n, k+1)
 
 
 def main():
-	
-	return 0
+    print largest_prime_factor(13195)
 
 if __name__ == '__main__':
-	main()
+    main()
 
