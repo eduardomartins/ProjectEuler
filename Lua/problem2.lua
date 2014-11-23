6e56a85b417bc0dc35438c381cfffa219278fb9e
Eduardo Sant'Anna Martins <eduardomartins993@hotmail.com>
2014-11-23 15:48:24 -0200

    Segundo exercício

---
+++ Lua/problem2.lua
@@ -0,0 +1,8 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: eduardo
+-- Date: 23/11/14
+-- Time: 15:37
+-- To change this template use File | Settings | File Templates.
+--
+
---
+++ Lua/problem3.lua
@@ -0,0 +1,8 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Eduardo
+-- Date: 23/11/14
+-- Time: 15:38
+-- To change this template use File | Settings | File Templates.
+--
+
