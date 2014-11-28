f350cd953dbc06632de4f5f3024cc257b1327888
Eduardo Sant'Anna Martins <eduardomartins993@hotmail.com>
2014-11-28 14:23:26 -0200

    Solução do problema 4 !!!\o/

---
+++ Python/problem4.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  problem4.py
+#  
+#  Copyright 2014 Eduardo Sant Anna Martins <eduardomartins993@hotmail.com>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  ==================================================================
+#  
+
+
+def palindromic(x, y):
+    return str(x * y) == str(x * y)[::-1]
+
+
+def largest_palindromic(n=100, k=999):
+    num = 0
+    for i in xrange(k, n, -1):
+        for x in xrange(k, n, -1):
+            if palindromic(i, x) and i * x > num:
+                num = i * x
+    return num
+
+
+def main():
+    print largest_palindromic()
+    return 0
+
+if __name__ == '__main__':
+    main()
+
