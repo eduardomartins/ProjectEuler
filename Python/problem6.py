f891bd27ccc55dd17dc44751d915db50a01e616e
Eduardo Sant'Anna Martins <eduardomartins993@hotmail.com>
2014-12-01 10:30:21 -0200

    Ajustes

--- Python/problem6.py
+++ Python/problem6.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#  sem título.py
+#  problem6.py
 #  
 #  Copyright 2014 Eduardo Sant Anna Martins <eduardomartins993@hotmail.com>
 #  
@@ -19,15 +19,35 @@
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-#  
-#  
+#  ==================================================================
+#  Sum square difference
+#  Problem 6
+#
+#  The sum of the squares of the first ten natural numbers is,
+#               1² + 2² + ... + 10² = 385
+#
+#  The square of the sum of the first ten natural numbers is,
+#            (1 + 2 + ... + 10)² = 55² = 3025
+#
+#  Hence the difference between the sum of the squares of the first ten
+#  natural numbers and the square of the sum is 3025 − 385 = 2640.
+#  Find the difference between the sum of the squares of the first one
+#  hundred natural numbers and the square of the sum.
+
+
+def get_term(n):
+    if n <= 1:
+        return
+    else:
+        return get_term(n-1) + (2*n - 1)
 
 
 
 def main():
-	
-	return 0
+    for i in range(1, 11):
+        print get_term(i)
+    return 0
 
 if __name__ == '__main__':
-	main()
+    main()
 
