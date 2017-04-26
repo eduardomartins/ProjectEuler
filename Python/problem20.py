bab7c7d5d2dbd6c45db90f6c657390d66b1bdb54
Eduardo Sant'Anna Martins <eduardo@eduardomartins.site>
2017-04-26 12:31:43 -0300

    Solve problem 20

---
+++ Python/problem20.py
@@ -0,0 +1,46 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  problem20.py
+#
+#  Copyright 2017 Eduardo Sant'Anna Martins <eduardo@eduardomartins.site>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#  ==========================================================
+# Factorial digit sum
+#
+# n! means n × (n − 1) × ... × 3 × 2 × 1
+# For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+# and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+# Find the sum of the digits in the number 100!
+
+
+def factorial(n):
+    result = 1
+    while n > 0:
+        result *= n
+        n -= 1
+    return result
+
+
+def main():
+    num = sum([int(n) for n in str(factorial(100))])
+    print num
+
+
+if __name__ == '__main__':
+    main()
